be361086192ee87f1aacf5ceecb6d0d0a5df1038
Sean Mizen <sean@seanmizen.com>
2017-05-09 15:33:50 +0100

    First commit. Blank Main class with minimal comments

---
+++ src/gameoflife/GameController.java
@@ -0,0 +1,20 @@
+package gameoflife;
+
+/**
+ * @author Sean Mizen
+ * Game of life: Rules based matrix application.
+ */
+public class GameController {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //-Create Grid class
+        //-Create Cell class (For expansion reasons)
+        //
+        //-Add "Graphics" Package from Arantza
+    }
+    
+}
